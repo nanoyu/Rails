a63bb3995a769dbce664c6986bcc1de53d03158a
永野裕也 <naganoyuuya@yuuya.local>
2018-11-12 20:31:52 +0900

    [finish]3-8

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '63285bb1249d9f36216f666b31cdee45b1ab0ffc52c033d1eb26e7c1e539d642fa81dac6cc86164bceac4d18c813e88395dc6bf24fc6cfe6c23f3d81a55b0aac'
